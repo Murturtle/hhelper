e1236a0a309696197281299102fac7f9aadde2e2
Murturtle <97775022+Murturtle@users.noreply.github.com>
2024-10-10 23:29:28 +0000

    a

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 const express = require('express')
+const canvasAPI = require('node-canvas-api')
 const app = express()
 const port = 3000
+canvasAPI.getAccountIds()
+
 
 app.use('/app',express.static('public'))
 
