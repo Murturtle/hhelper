acaa7139dd483cf55795b694b0c23a130d19ddd8
Murturtle <97775022+Murturtle@users.noreply.github.com>
2024-11-08 00:06:55 +0000

    womp womp

--- src/index.js
+++ src/index.js
@@ -1,20 +1,51 @@
 const app = require('express')();
 const express = require('express');
 const server = require('http').createServer(app);
-const io = require('socket.io')(server);
-
+const io = require('socket.io')(server, {
+  path: '/socket.io/',
+  serveClient: true,
+  cors: {
+      origin: "*",
+      methods: ["GET", "POST"]
+  }
+});
 
 const port = 3000;
 
-app.use('/app',express.static('public'))
+var users = {}
+
+app.use('/app', express.static('public'));
 
 io.on('connection', (socket) => {
   console.log('user connected');
-  socket.on('disconnect', function () {
-    console.log('user disconnected');
-  });
-})
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  socket.on("key",function(data){
+    console.log(data);
+    
+
+    fetch("https://bsd.instructure.com/api/v1/users/self/favorites/courses?access_token="+data+"&per_page=99999").then((req) => {
+    try{
+      req.json().then((json) => {
+        const classes = {}
+        console.log(json)
+        for( key in json ){
+          console.log(json[key].name)
+          classes[json[key].id] = {
+            "name":json[key].name,
+            "id":json[key].id,
+          }
+          socket.join(json[key].id)
+        }
+        socket.emit("classes",classes)
+        
+      })
+    } catch (err){
+      console.log(err)
+    }
+    })
+  })
+});
+
+server.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
--- src/public/main.js
+++ src/public/main.js
@@ -1,30 +1,57 @@
 function init(){
-    const socket = io("wss://solid-invention-x6j7qqx66ghp59g-3000.app.github.dev/");
-    loginBox = new WinBox("Modal Window", {
+    const socket = io({
+        path: '/socket.io/',
+        transports: ['websocket', 'polling']
+    });
+    
+    const loginBox = new WinBox("Login", {
         title: "Enter API Key",
         modal: true,
         class: ["no-close"],
-        
     });
-
-    apiKeyBox = document.createElement("input");
+    
+    loginBox.width = loginBox.height
+    
+    const apiKeyBox = document.createElement("input");
     apiKeyBox.style.position = "absolute";
     apiKeyBox.style.top = "50%";
     apiKeyBox.style.left = "50%";
     apiKeyBox.style.transform = "translate(-50%,-50%)";
     apiKeyBox.style.padding = "15px";
-    apiKeyBox.disabled = "true";
+    apiKeyBox.style.width = "65%"
+    apiKeyBox.disabled = true;  // Changed to boolean
     loginBox.body.appendChild(apiKeyBox);
-
-    apiKeyBox.addEventListener("keydown",function(e){
-        if(e.key = "Enter"){
-
+    
+    
+    apiKeyBox.addEventListener("keydown", function(e) {
+        if(e.key === "Enter") {  
+            socket.emit("key",apiKeyBox.value);
+            apiKeyBox.disabled = true;
         }
-    })
-
-    socket.on("connection", function(e){
-        apiKeyBox.disabled = "false"
+    });
+    
+    socket.on("connect", function() {
+        console.log("connected");
+        apiKeyBox.disabled = false;
+    });
+    
+    socket.on("classes", (data)=>{
+        console.log(data);
+        for(key in data){
+            const classBox = document.createElement("button");
+            classBox.innerText = data[key].name;
+            classBox.style.display = "block";
+            classBox.style.margin = "5px";
+            classBox.style.fontSize = "16px";
+            classBox.style.background = "#FFFFFF";
+            classBox.style.border = "solid 1px black"
+            classBox.style.padding = "10px";
+            classBox.style.borderRadius = "5px";
+            classSelector.appendChild(classBox)
+        }
+        loginBox.close();
     })
 }
 
-window.addEventListener("load",init)
+
+document.addEventListener("load",init())
